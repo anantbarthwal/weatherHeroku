7b180f950dc212562895f38864f181b40c839929
anantbarthwal <anantbarthwal@gmail.com>
2021-11-21 09:32:59 +0530

    first commit

---
+++ src/test/java/com/rogue/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogue.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
